3ea06a1eb1abd1d0bf4d7e1ee65d1446e9a0aa13
lzh <13247313874@163.com>
2023-04-20 15:55:15 +0800

    webscokt对接

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "@chatui/core/es/styles/index.less";
+// 引入组件
+import Chat, { Bubble, useMessages } from "@chatui/core";
+// 引入样式
+import "@chatui/core/dist/index.css";
+import "./App.css";
+
+// const ws = new WebSocket(
+//   "ws://gpt.imarin.cn:9999/api/websocket/Bearer chatgpt-12893127ghjasflgas89124ghwqdf7812dh2178ed"
+// );
+
+// ws.addEventListener("open", () => {
+//   console.log(11);
+// });
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { messages, appendMsg, setTyping } = useMessages([]);
+  const ws = useRef<WebSocket | null>(null);
+
+  // const [timer, setTimer] = useState(null);
+  const [msg, setMsg] = useState("");
+  let timer: any = null;
+  let msgs = "";
+  let time = 0;
+
+  useLayoutEffect(() => {
+    ws.current = new WebSocket(
+      "ws://gpt.imarin.cn:9999/api/websocket/Bearer chatgpt-12893127ghjasflgas89124ghwqdf7812dh2178ed"
+    );
+    ws.current.onmessage = (res) => {
+      clearTimeout(timer);
+      timer = null;
+      const data = JSON.parse(res.data);
+      if (data.code === 200) {
+        msgs += data.data;
+      }
+      timer = setTimeout(() => {
+        appendMsg({
+          type: "text",
+          content: { text: msgs },
+        });
+        msgs = "";
+        timer = null;
+      }, 500);
+    };
+  });
+
+  function handleSend(type: string, val: string) {
+    console.log(val);
+    if (type === "text" && val.trim()) {
+      appendMsg({
+        type: "text",
+        content: { text: val },
+        position: "right",
+      });
+      ws.current?.send(val);
+      setTyping(true);
+    }
+  }
+
+  function renderMessageContent(msg: any) {
+    const { content } = msg;
+    return <Bubble content={content.text} />;
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Chat
+      navbar={{ title: "美女在线陪聊" }}
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      onSend={handleSend}
+    />
+  );
 }
 
-export default App
+export default App;
